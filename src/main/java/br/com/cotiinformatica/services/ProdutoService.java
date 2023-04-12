01406a05158b6553ec02b1084180f03be207326c
ThiagoLizardo <thiagolizardo.dev@gmail.com>
2023-04-12 17:29:04 -0300

    Aula 8 - Arquiteto Java

--- src/main/java/br/com/cotiinformatica/services/ProdutoService.java
+++ src/main/java/br/com/cotiinformatica/services/ProdutoService.java
@@ -1,5 +1,35 @@
 package br.com.cotiinformatica.services;
 
-public class ProdutoService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.dtos.commands.ProdutoCreateCommand;
+import br.com.cotiinformatica.dtos.commands.ProdutoDeleteCommand;
+import br.com.cotiinformatica.dtos.commands.ProdutoUpdateCommand;
+import br.com.cotiinformatica.dtos.queries.ProdutosDto;
+
+@Service
+public class ProdutoService {
+	
+	public void create(ProdutoCreateCommand command) {
+		
+	}
+
+	public void update(ProdutoUpdateCommand command) {
+		
+	}
+	
+	public void delte(ProdutoDeleteCommand command) {
+		
+	}	
+	
+	public List<ProdutosDto> findAll(){
+		return null;
+	}
+	
+	public ProdutosDto findById(Integer id) {
+		return null;
+	}
+	
 }
